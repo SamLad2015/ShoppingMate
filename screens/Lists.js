fc2959d9fdb5e09c9b78c8d0cd7e734e5c77b897
sam <lad.sangram@gmail.com>
2020-06-25 02:12:11 +0100

    styled up lists screen

--- screens/Lists.js
+++ screens/Lists.js
@@ -43,22 +43,18 @@ class Lists extends Component {
             <View style={globalStyles.container}>
                 <ImageBackground source={image} style={globalStyles.bgImage}>
                     <Text style={globalStyles.heading}>Shopping List</Text>
-                    <View style={globalStyles.listWrapper}>
+                    <View style={styles.listWrapper}>
                         <FlatList data={lists.lists}
                                   renderItem={({item}) =>
-                                      <View style={globalStyles.itemRow}>
-                                          <View style={globalButtons.counterButtonWrapper}>
-                                              <Text style={globalStyles.listLabel}>{item.label}</Text>
-                                              <TouchableOpacity style={globalButtons.redButton}
-                                                                onPress={() => {
-                                                                    setList(item)
-                                                                    navigate('List', {
-                                                                        listId: item.id
-                                                                    })
-                                                                }}>
-                                                  <Text style={globalButtons.redButtonText}>>></Text>
-                                              </TouchableOpacity>
-                                          </View>
+                                      <View style={styles.itemRow}>
+                                          <TouchableOpacity style={globalButtons.counterButtonWrapper} onPress={() => {
+                                                                setList(item)
+                                                                navigate('List', {
+                                                                    listId: item.id
+                                                                })
+                                                            }}>
+                                              <Text style={[globalStyles.listLabel, styles.listLable]}>{item.label}</Text>
+                                          </TouchableOpacity>
                                       </View>
                                   }
                         />
@@ -79,6 +75,31 @@ class Lists extends Component {
         );
     }
 }
+const styles = StyleSheet.create({
+    itemRow: {
+        flexDirection: 'row',
+        paddingLeft: 20,
+        paddingBottom: 10,
+        marginBottom: 15,
+        borderBottomColor: '#c0c0c0',
+        borderBottomWidth: 1,
+        backgroundColor: '#000',
+        borderRadius: 10
+    },
+    listWrapper: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        marginTop: 100,
+        marginBottom: 75,
+        paddingLeft: 10,
+        paddingRight: 10
+    },
+    listLable: {
+        fontSize: 25,
+        color: '#fff'
+    }
+});
 const mapStateToProps = state => ({
     lists: state.lists,
     list: state.list
