75506e1b38d455c0907d17f72cacc1f6d5e51b64
sam <lad.sangram@gmail.com>
2020-07-24 23:37:18 +0100

    added accept mate logic

--- screens/Lists/Lists.js
+++ screens/Lists/Lists.js
@@ -13,7 +13,7 @@ import {connect} from "react-redux";
 import {globalStyles, globalButtons, iconStyles, subHeaderStyles, swipeStyles} from '../../styles/Styles';
 import * as _ from "lodash";
 import {removeList, setList} from "../../actions/lists";
-import {setUser, setMates, setRequests} from "../../actions/mates";
+import {setUser, setMates, setRequests, setFriends} from "../../actions/mates";
 import moment from "moment";
 import Header from "../Header";
 import ItemsService from "../../services/itemsService";
@@ -30,8 +30,7 @@ class Lists extends Component {
         super(props);
         this.state = {
             activeRow : null,
-            matesLoading: false,
-            mates: []
+            matesLoading: false
         };
     }
     static navigationOptions = ({ navigation }) =>  ({
@@ -107,17 +106,24 @@ class Lists extends Component {
     getMates(uid) {
         const promises = this.getMatesRequests(uid);
         this.setState({matesLoading : true});
-        const {setMates, setRequests} = this.props;
+        const {setMates, setRequests, setFriends} = this.props;
         return promises.then(matesPromises => {
-            this.setState({mates: matesPromises[0], requests: matesPromises[1], matesLoading: false});
-            setMates(matesPromises[0]);
-            setRequests(matesPromises[1]);
+            this.setState({
+                friends:matesPromises[1],
+                invites: matesPromises[2],
+                requests: matesPromises[0],
+                matesLoading: false
+            });
+            setFriends(matesPromises[1]);
+            setMates(matesPromises[2]);
+            setRequests(matesPromises[0]);
         });
     }
     async getMatesRequests(uid) {
         const promises = [];
         const fbService = new FirebaseService();
-        promises.push(await fbService.searchItem('mates', 'mateUid', '==', uid, true));
+        promises.push(await fbService.getInvites(uid));
+        promises.push(await fbService.getMates(uid));
         promises.push(await fbService.getRequests(uid));
         return promises;
     }
@@ -273,4 +279,4 @@ const mapStateToProps = state => ({
     list: state.list,
     uid: state.uid
 });
-export default connect(mapStateToProps, {setList, removeList, setUser, setMates, setRequests})(Lists)
+export default connect(mapStateToProps, {setList, removeList, setUser, setMates, setRequests, setFriends})(Lists)
